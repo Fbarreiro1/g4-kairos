574cf58eb1e7499f887d6e2ba90566fad018d29a
Federico Barreiro <fed_eh@hotmail.com>
2024-07-13 14:42:37 -0300

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,83 +1,72 @@
-// App.js
-import React, { useState, useEffect } from 'react';
-import { fetchData } from '../../funciones/Api';
-import './Login.css';
-import { useNavigate } from 'react-router-dom';  
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import avatar from '../../media/avatar.png';
+import { fetchData } from '../../funciones/Api'; // Importa la función fetchData si aún no lo has hecho
 import { guardarDato } from '../../funciones/localStorageUtils.js';
 
-
-
 function Login() {
-  const [data, setData] = useState([]);
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     USERNAME: '',
     PASSWORD: ''
   });
 
-
-
-  const fetchDataAndUpdate = () => {
-    fetchData('usuarios')
-      .then(data => setData(data))
-      .catch(error => console.error('Error:', error));
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
-  useEffect(() => {
-    fetchDataAndUpdate();
-  }, []);
-  
-  const handleChange = (e) =>{
-    const {name, value} = e.target;
-    setFormData({...formData, [name]: value});
-  }
-
-  
-  const handleLogin = async (e)=>{
+  const handleLogin = async (e) => {
     e.preventDefault();
-    try{
-        await fetchData('usuarios');
-        const usuarioRegistrado = data.find(usuario => usuario.USERNAME === formData.USERNAME);
-        if(!usuarioRegistrado){
-          alert('El usuario no está registrado');
-        } else{
-          if (usuarioRegistrado.PASSWORD === formData.PASSWORD){
-            guardarDato('USERNAME', usuarioRegistrado.USERNAME);
-            guardarDato('tipo', usuarioRegistrado.TIPO);
-            guardarDato('campo', usuarioRegistrado.CAMPO);
-            guardarDato('clinica', usuarioRegistrado.FK_CLINICAS);
-            navigate("/home");
-          }else{  
-            alert('Contraseña incorrecta');
-          }
+
+    try {
+      // Obtener la lista de usuarios del backend
+      const data = await fetchData('usuarios');
+      const usuarioRegistrado = data.find(usuario => usuario.USERNAME === formData.USERNAME);
+
+      if (!usuarioRegistrado) {
+        // Si el usuario no está registrado, verificar si es el usuario de acceso directo
+        if (formData.USERNAME === 'user' && formData.PASSWORD === '123') {
+          // Autenticación exitosa para usuario 'user'
+          guardarDato('USERNAME', 'user'); // Guardar en localStorage si es necesario
+          navigate('/home'); // Redirigir al usuario a la página de inicio
+        } else {
+          // Mostrar mensaje de usuario no registrado
+          alert('El usuario no está registrado o los datos ingresados son incorrectos.');
         }
-    }
-    catch (error){
-        console.error('Error', error);
-        alert('Error de servidor');
+      } else {
+        // Verificar la contraseña ingresada con la del usuario registrado
+        if (usuarioRegistrado.PASSWORD === formData.PASSWORD) {
+          // Autenticación exitosa para usuario registrado
+          guardarDato('USERNAME', usuarioRegistrado.USERNAME); // Guardar en localStorage si es necesario
+          navigate('/home'); // Redirigir al usuario a la página de inicio
+        } else {
+          // Mostrar mensaje de contraseña incorrecta
+          alert('Contraseña incorrecta');
+        }
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error de servidor');
     }
   };
-
-
 
   return (
     <div className='pageLogin'>
-        <div className='loginContainer'>
-          <img className='avatarLog' src={avatar} alt='avatar'/>
-      <form onSubmit={handleLogin} className='formLogContainer'>
-      <label className='labelLog'>
-        Nombre de usuario:
-        <input type="text" name="USERNAME" value={formData.USERNAME} onChange={handleChange} />
-      </label>
-      <label className='labelLog'>
-        Contraseña:
-        <input type="PASSWORD" name="PASSWORD" value={formData.PASSWORD} onChange={handleChange} />
-      </label>
-      <button type="submit" className='botLog'>INGRESAR</button>
-    </form>
-    </div>
-
+      <div className='loginContainer'>
+        <img className='avatarLog' src={avatar} alt='avatar' />
+        <form onSubmit={handleLogin} className='formLogContainer'>
+          <label className='labelLog'>
+            Nombre de usuario:
+            <input type="text" name="USERNAME" value={formData.USERNAME} onChange={handleChange} />
+          </label>
+          <label className='labelLog'>
+            Contraseña:
+            <input type="password" name="PASSWORD" value={formData.PASSWORD} onChange={handleChange} />
+          </label>
+          <button type="submit" className='botLog'>INGRESAR</button>
+        </form>
+      </div>
     </div>
   );
 }
