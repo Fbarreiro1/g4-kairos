2d4d3c1bfd297b12521fd627517f63648e1f8e54
Federico Barreiro <fed_eh@hotmail.com>
2024-07-23 13:31:58 -0300

    Update config.js

--- g4-back/config.js
+++ g4-back/config.js
@@ -2,7 +2,7 @@ module.exports = {
   F: process.env.F || 'localhost',
   DB_USER: process.env.DB_USER || 'root',
   DB_PASSWORD: process.env.DB_PASSWORD || '1234',
-  DB_NAME: process.env.DB_NAME || 'clinica_db',
+  
   DB_PORT: process.env.DB_PORT || 3306,
   PORT: process.env.PORT || 3000,
   MYSQL_URL: process.env.MYSQL_URL // Correcto: sin espacio después de `process.env.`
