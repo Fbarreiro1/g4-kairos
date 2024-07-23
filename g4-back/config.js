92b030bc81033d845ec3c6c3a47eeb24ba27383b
Federico Barreiro <fed_eh@hotmail.com>
2024-07-23 13:24:59 -0300

    Update config.js

--- g4-back/config.js
+++ g4-back/config.js
@@ -5,5 +5,5 @@ module.exports = {
   DB_NAME: process.env.DB_NAME || 'clinica_db',
   DB_PORT: process.env.DB_PORT || 3306,
   PORT: process.env.PORT || 3000,
-  MSQUL_URL: process.env.MYSQL_URL // Correcto: sin espacio después de `process.env.`
+  MYSQL_URL: process.env.MYSQL_URL // Correcto: sin espacio después de `process.env.`
 };
