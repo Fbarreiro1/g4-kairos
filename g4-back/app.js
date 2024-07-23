639dbb3cbce2ea347722c6d31aba7ca2c3f82e79
Federico Barreiro <fed_eh@hotmail.com>
2024-07-23 12:53:19 -0300

    Update app.js

--- g4-back/app.js
+++ g4-back/app.js
@@ -16,7 +16,7 @@
 
   // MySQL Connection
   const connection = mysql.createConnection({
-    host: "viaduct.proxy.rlwy.net",
+    host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
