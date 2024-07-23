2862dafa805ed293cb61699b98579b7f90eeda68
Federico Barreiro <fed_eh@hotmail.com>
2024-07-23 13:01:17 -0300

    Update config.js

--- g4-back/config.js
+++ g4-back/config.js
@@ -4,5 +4,6 @@ module.exports = {
     DB_PASSWORD: process.env.DB_PASSWORD || '1234',
     DB_NAME: process.env.DB_NAME || 'clinica_db',
     DB_PORT: process.env.DB_PORT || 3306,
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    MYSQL_URL: process.env. MYSQL_URL 
   };
